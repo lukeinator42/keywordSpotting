619ce8d2933a23d20204ca50f2afbe955ee3b8e4
lukeinator42 <lukas.grasse@uleth.ca>
2019-06-04 11:10:35 -0600

    write file

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <keywordSpotter/EmbeddingNetwork.h>
 #include <sndfile.h>
 #include <keywordSpotter/utils/WavUtils.h>
@@ -13,12 +14,14 @@ int main(int argc, char ** argv) {
 
     //cout << e.getMsg() << endl;
     
-    if (argc != 2) {
+    if (argc != 3) {
         printf("\nUsage :\n\n    <executable name>  <input file>\n") ;
         exit(0);
     }
 
-    string path(argv[1]);
+    string inpath(argv[1]);
+
+    string outpath(argv[2]);
 
     WavUtils wavUtils;
 
@@ -30,23 +33,29 @@ int main(int argc, char ** argv) {
     double minFreq = 50;
     double maxFreq = 8000;
     double sampleRate = 16000;
-    bool logScale = false;
+    bool logScale = true;
 
     Spectrogram spectrogram(nFFT, winLen, hopSize, minFreq, maxFreq, sampleRate, logScale);
 
     // cout << buffer.size() << endl;
 
-    wavUtils.readFile(path, buffer);
+    wavUtils.readFile(inpath, buffer);
 
     // cout << buffer.size() << endl;
 
     vector< vector<double> > s = spectrogram.computeSpectrogram(buffer.getAudio());
 
+
+    ofstream fout;
+    fout.open(outpath);
+
     for(int i=0; i<s.size(); i++) {
         for(int j=0; j<s[i].size(); j++)
-            cout << s[i][j] << " ";
-        cout << endl;
+            fout << s[i][j] << " ";
+        fout << endl;
     }
+
+    fout.close();
     
     return 0;
 }
